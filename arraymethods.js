72838a4fb24be82a84eea7cb478e846d7c425187
lapinell <lapinell@gmail.com>
2018-02-16 12:15:18 -0600

    added base files

---
+++ arraymethods.js
@@ -0,0 +1,10 @@
+// Create three separate arrays for songs, albums and artists
+// The song array should be initialized with 10 songs, and for each subsequent array should hold the corresponding data
+//Each  song is from a unique artist and album
+let songs = [];
+let artists = [];
+let albums = [];
+
+let songsSection = document.getElementById("songs");
+let artistsSection = document.getElementById("artists");
+let albumSection = document.getElementById("albums");
