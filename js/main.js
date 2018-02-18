a77bf6e6aeb570d71073e2277021c51792612698
lapinell <lapinell@gmail.com>
2018-02-17 18:48:19 -0600

    added module plan to main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@
 // The song array should be initialized with 10 songs, and for each subsequent array should hold the corresponding data
 //Each  song is from a unique artist and album
 
-let songs = ["Olivia", "The Scientist", "Feels", "Skinny Love", "Dusk Till Dawn", "Inside Out", "Do I Wanna Know?", "What About Us", "Redbone", "Dropping"];
-let artists = ["Shigeto", "Coldplay", "Calvin Harris", "Bon Iver", "Zayn", "Camila Cabello", "Artic Monkeys", "P!nk", "Childish Gambino", "Yosi Horikawa"];
-let albums = ["No Better Time Than .", "A Rush of Blood to the Head", "Funk Wav Bounces Vol. 1", "For Emma, Forever Ago", "Dusk Till Dawn Single", "Camila", "AM", "Beautiful Trauma", "Awaken, My Love!", "Touch"];
+let songs = ["Olivia", "The Scientist", "Feels", "Skinny Love", "Dusk Till Dawn", "Inside Out", "Do I Wanna Know?", "Send and Receive (Chachi Jones Remix)", "Hana", "Dropping"];
+let artists = ["Shigeto", "Coldplay", "Calvin Harris", "Bon Iver", "Zayn", "Camila Cabello", "Artic Monkeys", "Tycho", "Thomas Fehlmann", "Yosi Horikawa"];
+let albums = ["No Better Time Than .", "A Rush of Blood to the Head", "Funk Wav Bounces Vol. 1", "For Emma, Forever Ago", "Dusk Till Dawn Single", "Camila", "AM", "Past is Prologue", "LoveFlow", "Touch"];
 let suggested = [false, false, false, false, false, false, false, false, false, false];
 
 let songsSection = document.getElementById("songs");
@@ -20,7 +20,19 @@ console.log(songs, artists, albums, suggested);
 
 let display = require("./_display");
 
+///Print to DOM
 display.printArray(songs, songsSection) ;
 display.printArray(artists, artistsSection) ;
 display.printArray(albums, albumSection) ;
 display.printArray(suggested, suggestedSection) ;
+
+///Invert the Data vs Not
+
+///Ascending & Descending
+
+///Vertical vs Horizontal
+
+///Add modification under h1 tag as h2
+
+///Add song to array w/o refreshing page
+
