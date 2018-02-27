3830e1ea9581cf4966b3de95d07cf9b7dc43a22b
lapinell <lapinell@gmail.com>
2018-02-27 17:54:51 -0600

    assigned variables to radio IDs

--- js/_switch.js
+++ js/_switch.js
@@ -1,2 +1,7 @@
 "use strict";
 
+let asc = document.getElementById("asc");
+let desc = document.getElementById("desc");
+let none = document.getElementById("none");
+let vert = document.getElementById("vert");
+let horz = document.getElementById("horz");
