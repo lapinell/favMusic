e6ea3d0da0f83d4cf3d8c51259c37f3c06906231
lapinell <lapinell@gmail.com>
2018-02-27 18:25:00 -0600

    moved data to own module so could access easier from all other modules

---
+++ js/_data.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let songsArray = ["Olivia", "The Scientist", "Feels", "Skinny Love", "Dusk Till Dawn", "Inside Out", "Do I Wanna Know?", "Send and Receive (Chachi Jones Remix)", "Hana", "Dropping"];
+let artistsArray = ["Shigeto", "Coldplay", "Calvin Harris", "Bon Iver", "Zayn", "Camila Cabello", "Artic Monkeys", "Tycho", "Thomas Fehlmann", "Yosi Horikawa"];
+let albumsArray = ["No Better Time Than .", "A Rush of Blood to the Head", "Funk Wav Bounces Vol. 1", "For Emma, Forever Ago", "Dusk Till Dawn Single", "Camila", "AM", "Past is Prologue", "LoveFlow", "Touch"];
+let suggestedArray = [false, false, false, false, false, false, false, false, false, false];
+
+let songsSection = () => { return document.getElementById("songs");
+};
+
+let artistsSection = () => { return document.getElementById("artists");
+};
+
+let albumSection = () => { return  document.getElementById("albums");
+};
+
+let suggestedSection  = () => { return document.getElementById("suggested");
+};
+
+let songs = () => {
+    return songsArray;
+};
+let artists = () => {
+    return artistsArray;
+};
+let albums = () => {
+    return albumsArray;
+};
+let suggested = () => {
+    return suggestedArray;
+};
+
+module.exports = { songs, artists, albums, songsSection, artistsSection, suggestedSection, albumSection, suggested};
