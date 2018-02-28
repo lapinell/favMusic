896e07a355795345a3436277ddba340326d49a75
lapinell <lapinell@gmail.com>
2018-02-27 18:25:59 -0600

    modified to add requirement for data.js

--- js/_switch.js
+++ js/_switch.js
@@ -1,14 +1,24 @@
 "use strict";
 
+let data = require('./_data.js');
+let display = require('./_display.js');
+let modify = require('./_modify.js');
+
 let asc = document.getElementById("asc");
 let desc = document.getElementById("desc");
 let none = document.getElementById("none");
 let vert = document.getElementById("vert");
 let horz = document.getElementById("horz");
 
-asc.addEventListener("change", function() {console.log('Asc selected');});
+//Radio Button Event Listeners
+asc.addEventListener("change", function() {console.log('asc selected');});
+
 desc.addEventListener("change", function() {console.log('desc selected');});
+
 none.addEventListener("change", function() {console.log('none selected');});
+
 vert.addEventListener("change", function() {console.log('vert selected');});
+
 horz.addEventListener("change", function() {console.log('horz selected');});
+
 
--- js/main.js
+++ js/main.js
@@ -3,18 +3,9 @@
 // Create three separate arrays for songs, albums and artists
 // The song array should be initialized with 10 songs, and for each subsequent array should hold the corresponding data
 //Each  song is from a unique artist and album
+let data = require('./_data.js');
 
-let songs = ["Olivia", "The Scientist", "Feels", "Skinny Love", "Dusk Till Dawn", "Inside Out", "Do I Wanna Know?", "Send and Receive (Chachi Jones Remix)", "Hana", "Dropping"];
-let artists = ["Shigeto", "Coldplay", "Calvin Harris", "Bon Iver", "Zayn", "Camila Cabello", "Artic Monkeys", "Tycho", "Thomas Fehlmann", "Yosi Horikawa"];
-let albums = ["No Better Time Than .", "A Rush of Blood to the Head", "Funk Wav Bounces Vol. 1", "For Emma, Forever Ago", "Dusk Till Dawn Single", "Camila", "AM", "Past is Prologue", "LoveFlow", "Touch"];
-let suggested = [false, false, false, false, false, false, false, false, false, false];
-
-let songsSection = document.getElementById("songs");
-let artistsSection = document.getElementById("artists");
-let albumSection = document.getElementById("albums");
-let suggestedSection = document.getElementById("suggested");
-
-console.log(songs, artists, albums, suggested);
+console.log(data.songs(), data.artists(), data.albums(), data.suggested());
 
 //Required Files
 
@@ -24,24 +15,6 @@ let change = require("./_switch");
 
 ///Print to DOM
 // display.verticalArray(songs, songsSection) ;
-display.verticalArray(artists, artistsSection) ;
-display.verticalArray(albums, albumSection) ;
-display.verticalArray(suggested, suggestedSection) ;
-
-///Invert the Data vs Not
-console.log('invertData Songs:', modify.invertData(songs));
-
-///Ascending & Descending
-
-console.log('ascendingData Songs:', modify.ascendingData(songs));
-console.log('descendingData artists:', modify.descendingData(artists));
-
-///Vertical vs Horizontal
-
-// display.horizontalArray(songs, songsSection);
-
-///Add modification description under h1 tag as h2
-
-
-///Add song to array w/o refreshing page
-
+display.verticalArray(data.artists(), data.artistsSection()) ;
+display.verticalArray(data.albums(), data.albumSection()) ;
+display.verticalArray(data.suggested(), data.suggestedSection()) ;
